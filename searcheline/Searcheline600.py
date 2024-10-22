e48c922de65b269cd1874b9deeca2be3c471d872
lordsnek <67084131+lordsnek@users.noreply.github.com>
2024-10-22 17:37:35 -0500

    Update Searcheline600.py
    Fixed commenting to not be outdated for no reason

--- searcheline/Searcheline600.py
+++ searcheline/Searcheline600.py
@@ -4,24 +4,17 @@ import CelesteUtils as utils
 class Search100(Searcheline):
   # initial state to search from
   def init_state(self):
-    utils.load_room(self.p8, 5) # load 100m
-    #utils.suppress_object(self.p8, self.p8.game.fake_wall) # don't consider berry block
+    utils.load_room(self.p8, 5) # load 600m
     utils.skip_player_spawn(self.p8) # skip to after player has spawned
-    # execute this list of initial inputs
-    #for a in [18, 2, 2, 2, 2, 2, 2, 2, 2, 2, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]:
-      #self.p8.set_btn_state(a)
-      #self.p8.step()
-    # alternatively, using output from a TAS tool:
-    # utils.place_maddy(self.p8, 55, 79, 0.2, 0.185, 1.4, 0.63, 0, 0)
     return self.p8.game.objects
 
-  # get list of available inputs for a state - only consider {r, r + z, u + r + x}
+  # get list of available inputs for a state - only consider {r, r + z, u + r + x, r + x}
   def allowable_actions(self, objs, player, h_movement, can_jump, can_dash):
     actions = [0b000010] # r
     if can_jump:
       actions.extend([0b010010]) # r + z
     if can_dash:
-      actions.extend([0b100010, 0b100110]) # r + x, u + x, u + r + x
+      actions.extend([0b100010, 0b100110]) # r + x, u + r + x
     return actions
 
 if __name__ == '__main__':
