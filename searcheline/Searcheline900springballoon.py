b80b480d1e1a835055b08342671b0fb60c584ad8
lordsnek <67084131+lordsnek@users.noreply.github.com>
2024-10-23 16:13:57 -0500

    Update Searcheline900springballoon.py
    Fixed commenting and changed r+z to l+z (no practical effect)

--- searcheline/Searcheline900springballoon.py
+++ searcheline/Searcheline900springballoon.py
@@ -5,15 +5,10 @@ import math
 class Search100(Searcheline):
   # initial state to search from
   def init_state(self):
-    utils.load_room(self.p8, 8) # load 400m
-    #utils.suppress_object(self.p8, self.p8.game.balloon) # don't consider berry block
-    utils.suppress_object(self.p8, self.p8.game.fake_wall)
+    utils.load_room(self.p8, 8) # load 900m
+    utils.suppress_object(self.p8, self.p8.game.fake_wall) # don't consider berry block
     utils.skip_player_spawn(self.p8) # skip to after player has spawned
-    # execute this list of initial inputs
-    #for a in [18, 2, 2, 2, 2, 2, 2, 2, 2, 2, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]:
-      #self.p8.set_btn_state(a)
-      #self.p8.step()
-    # alternatively, using output from a TAS tool:
+    # using output from a TAS tool:
     utils.place_maddy(self.p8, 92, 96, -.45, -0.0804443, -1, -2.79, 0, 1)
     return self.p8.game.objects
 
@@ -30,30 +25,21 @@ class Search100(Searcheline):
 
   def is_goal(self, objs):
     p = self.find_player(objs)
-    return p and p.y<40 and p.djump>0 #Be at the height of the final cloud, without going far to the right
+    return p and p.y<40 and p.djump>0 # Be in the top third of the level with a dash
 
   # get list of available inputs for a state - only consider {r, r + z, u + r + x}
   def allowable_actions(self, objs, player, h_movement, can_jump, can_dash):
     if h_movement and player.y>50:
      actions = [0b000001] # l
     else:
-      actions = ([0b000000])
+      actions = ([0b000000]) # n
     if can_jump:
-      actions.extend([0b010010]) # r + z
+      actions.extend([0b010001]) # l + z
     if can_dash:
-      actions.extend([0b100101, 0b100100]) # u + x, u + l + x
+      actions.extend([0b100101, 0b100100]) # u + l + x, u + x
     return actions
 
-  #def exit_heuristic(self, player, exit_spd_y=3):
-   #   if player.x<10:
-    #    return math.inf
-     # return 0
-
-  #def is_goal(self, objs):
-    #p = self.find_player(objs)
-    #return p and p.y<50 #Be able to walljump on a wall at the right height.
-
 if __name__ == '__main__':
-  # search up to depth 50, but stop at the depth of the first solution found
+  # search up to depth 526, and don't stop at the depth of the first solution found
   s = Search100()
   solutions = s.search(26,complete=True)
