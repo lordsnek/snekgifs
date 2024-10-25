3ee36ee51118f9397377eecfa7e8f90d5f5526c6
lordsnek <67084131+lordsnek@users.noreply.github.com>
2024-10-25 14:27:49 -0500

    Update Searcheline2600.py
    Fixed commenting etc

--- searcheline/Searcheline2600.py
+++ searcheline/Searcheline2600.py
@@ -6,16 +6,8 @@ class Search100(Searcheline):
   # initial state to search from
   def init_state(self):
     utils.set_max_djump(self.p8, 2)
-    utils.load_room(self.p8, 25) # load 400m
-    #utils.suppress_object(self.p8, self.p8.game.balloon) # don't consider berry block
-    #utils.suppress_object(self.p8, self.p8.game.chest)
+    utils.load_room(self.p8, 25) # load 2600m
     utils.skip_player_spawn(self.p8) # skip to after player has spawned
-    # execute this list of initial inputs
-    #for a in [18, 2, 2, 2, 2, 2, 2, 2, 2, 2, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]:
-      #self.p8.set_btn_state(a)
-      #self.p8.step()
-    # alternatively, using output from a TAS tool:
-    #utils.place_maddy(self.p8, 91, 96, 0.31041, 0.498535, 1, -0.53006, 0, 1)
     return self.p8.game.objects
 
   def h_cost(self, objs):
@@ -26,33 +18,23 @@ class Search100(Searcheline):
       target_y = 80
       max_spd_y = 4
       target_x = 62
-      max_spd_x = 4 # arbitrary large x heuristic to avoid paring solutions that don't work; x speed is useless for this search
+      max_spd_x = 4 
       return max(math.ceil(abs(player.y - target_y) / max_spd_y), math.ceil(abs(player.x-target_x)/max_spd_x))
 
   def is_goal(self, objs):
     p = self.find_player(objs)
-    return p and p.y==80 and p.spd.y+p.rem.y>-.5 #and p.djump>0 #Be at the height of the final cloud, without going far to the right
+    return p and p.y==80 and p.spd.y+p.rem.y>-.5 # be at the right height to clip on the first block
 
   # get list of available inputs for a state - only consider {r, r + z, u + r + x}
   def allowable_actions(self, objs, player, h_movement, can_jump, can_dash):
     actions = [0b000010] # r
-      #actions.extend([0b000000])
     if can_jump:
       actions.extend([0b010010]) # r + z
     if can_dash:
-      actions.extend([0b101010,0b100010,0b100100]) # u + x, u + l + x
+      actions.extend([0b101010,0b100010,0b100100]) # d + u + x, r + x, u + x
     return actions
 
-  #def exit_heuristic(self, player, exit_spd_y=3):
-   #   if player.x<10:
-    #    return math.inf
-     # return 0
-
-  #def is_goal(self, objs):
-    #p = self.find_player(objs)
-    #return p and p.y<50 #Be able to walljump on a wall at the right height.
-
 if __name__ == '__main__':
-  # search up to depth 50, but stop at the depth of the first solution found
+  # search up to depth 40, and don't stop at the depth of the first solution found
   s = Search100()
   solutions = s.search(40,complete=True)
